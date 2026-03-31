973d14977951d99ffbb3d8a6179da0db256ae126
TangSaiPang <89617575+TangSaiPang@users.noreply.github.com>
2026-04-01 00:13:45 +0800

    Update mux2.v

--- mux2.v
+++ mux2.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2026/03/25 22:51:50
-// Design Name: 
-// Module Name: mux2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module mux2(
 	a,
 	b,
