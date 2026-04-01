13ec20e591b9aa79309e37c7a075574aea83f22e
TangSaiPang <89617575+TangSaiPang@users.noreply.github.com>
2026-04-02 00:41:55 +0800

    Update led_twinkle_20240401.v

--- led_twinkle_20240401.v
+++ led_twinkle_20240401.v
@@ -1,75 +1,14 @@
-module led_twinkle(
+module led_twinkle_20240401(
     clk,
     rst_n,
     led
 );
 
-input clk;
-input rst_n;
-output wire [3:0]led;
-
-//reg [24:0]counter0;
-//always@(posedge clk or negedge rst_n)
-//    if(!rst_n)
-//        counter0 <= 0;
-//    else if(counter0 == 25_000_000 - 1)
-//        counter0 <= 0;
-//    else
-//        counter0 <= counter0 + 1'd1;
-
-//always@(posedge clk or negedge rst_n)
-//    if(!rst_n)
-//        led[0] <= 1'd0;
-//    else if(counter0 == 25_000_000 - 1)
-//        led[0] <= !led[0];
+    input clk;
+    input rst_n;
+    output wire [3:0]led;
 
 
-//reg [24:0]counter1;
-//always@(posedge clk or negedge rst_n)
-//    if(!rst_n)
-//        counter1 <= 0;
-//    else if(counter1 == 12_500_000 - 1)
-//        counter1 <= 0;
-//    else
-//        counter1 <= counter1 + 1'd1;
-
-//always@(posedge clk or negedge rst_n)
-//    if(!rst_n)
-//        led[1] <= 1'd0;
-//    else if(counter0 == 12_500_000 - 1)
-//        led[1] <= !led[1];
-
-
-//reg [24:0]counter2;
-//always@(posedge clk or negedge rst_n)
-//    if(!rst_n)
-//        counter2 <= 0;
-//    else if(counter2 == 6_250_000 - 1)
-//        counter2 <= 0;
-//    else
-//        counter2 <= counter2 + 1'd1;
-
-//always@(posedge clk or negedge rst_n)
-//    if(!rst_n)
-//        led[2] <= 1'd0;
-//    else if(counter0 == 6_250_000 - 1)
-//        led[2] <= !led[2];
-
-
-//reg [23:0]counter3;
-//always@(posedge clk or negedge rst_n)
-//    if(!rst_n)
-//        counter3 <= 0;
-//    else if(counter3 == 2_500_000 - 1)
-//        counter3 <= 0;
-//    else
-//        counter3 <= counter3 + 1'd1;
-
-//always@(posedge clk or negedge rst_n)
-//    if(!rst_n)
-//        led[3] <= 1'd0;
-//    else if(counter0 == 2_500_000 - 1)
-//        led[3] <= !led[3];
 
     led_flash led_twinkle_inst0(
         .clk(clk),
